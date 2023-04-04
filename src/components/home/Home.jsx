64b5faebc8c671b84c478b3ac913e7b43e898784
Shahin Rahimi <sh.rahimi.3d@gmail.com>
2023-04-04 23:09:43 +0300

    small changes

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -6,6 +6,11 @@ const Home = () => {
       <h3>Hi There, I'm Shahin</h3>
       <h1>Freelancer Developer</h1>
       <p>I love o code and helping people turn their ideas into life</p>
+      <ul className="home__icons">
+        <li className="home__icon"><a href="https://twitter.com/Rahimi3d"><i class="fa-brands fa-twitter"></i></a></li>
+        <li className="home__icon"><a href="https://github.com/shahinrahimi"><i class="fa-brands fa-github"></i></a></li>
+        <li className="home__icon"><a href="https://www.linkedin.com/in/shahinrahimideveloper/"><i class="fa-brands fa-linkedin"></i></a></li>
+      </ul>
     </section>
   )
 }
